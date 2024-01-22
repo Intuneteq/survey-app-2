f26c5979796fe3baedcb239936a38e5338450ddd
Intuneteq <tobiolanitori@gmail.com>
2024-01-22 16:02:36 +0000

    feat: image export implemented

---
+++ frontend/src/assets/images.ts
@@ -0,0 +1,9 @@
+// Import the image Like this
+import histogram from "./histogram.png";
+
+// Make sure us export it by passing it into this images object.
+const Images = {
+  histogram,
+};
+
+export default Images;
--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,7 +1,12 @@
-
+import { Images } from "../../assets";
 
 export default function Login() {
+  const { histogram } = Images;
+  
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Login</h1>
+      <img src={histogram} alt="histogram" />
+    </div>
+  );
 }
