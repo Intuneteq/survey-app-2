542b462955e13d397669344a67d0f03f0eb1d00f
Intuneteq <tobiolanitori@gmail.com>
2024-01-24 04:39:02 +0000

    refactor: auth layout responsiveness refactor

--- frontend/src/pages/auth/AuthLayout.tsx
+++ frontend/src/pages/auth/AuthLayout.tsx
@@ -8,8 +8,8 @@ export default function AuthLayout() {
   return (
     <main className="w-full h-screen bg-primary">
       <div className="flex w-full h-full">
-        <div className="flex justify-center items-end w-[60%] h-full bg-secondary rounded-tr-[2rem] rounded-br-[2rem] 2xl:rounded-tr-[6.2rem] 2xl:rounded-br-[6.2rem] py-[3.44rem] px-[6.56rem]">
-          <div className="w-[30rem] h-[25rem] 2xl:w-[43.125rem] 2xl:h-[38.125rem] flex justify-center items-center">
+        <div className="flex justify-center items-end w-[60%] h-full bg-secondary rounded-tr-[2rem] rounded-br-[2rem] xl:rounded-tr-[6.2rem] xl:rounded-br-[6.2rem] py-[3.44rem] px-[6.56rem]">
+          <div className="w-[30rem] h-[25rem] xl:w-[43.125rem] xl:h-[38.125rem] flex justify-center linerblue items-center">
             <img src={welcome} alt="welcome" className="w-full h-full object-contain" />
           </div>
         </div>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,20 +2,22 @@
 
 import defaultTheme from "tailwindcss/defaultTheme";
 
-
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['MuseoModerno', defaultTheme.fontFamily.sans],
+        sans: ["MuseoModerno", defaultTheme.fontFamily.sans],
       },
       colors: {
-        'primary': "#239D60",
-        'secondary': "#FFF5C1"
-      }
+        primary: "#239D60",
+        secondary: "#FFF5C1",
+      },
 
-
+      screens: {
+        xl: "1300px",
+        // => @media (min-width: 1300px) { ... }
+      },
       // fontSize: {},
       // colors: {},
     },
