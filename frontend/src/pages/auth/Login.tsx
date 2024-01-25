35afd22b27df9db527d03630d986e6b15c3e20ff
Christopher <odeniyichristopher7@gmail.com>
2024-01-25 01:01:09 +0000

    chore: restructuring login page using reusable components

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,13 +1,105 @@
 import { Images } from "../../assets";
 
-import { AuthButton } from "../../components/atoms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+
+import {
+  AuthButton,
+  SocialButton,
+  FormInput,
+  EnvelopeIcon,
+  PadlockIcon,
+} from "../../components/atoms";
+import { AuthFooter, AuthHeader } from "../../components/molecules";
 
 export default function Login() {
   const { google, apple } = Images;
 
   return (
     <>
-      <div className="flex-col flex justify-center items-center mt-[6.88rem] w-full">
+      <section>
+        <div className="flex-col flex justify-center items-center mt-[6.88rem] w-full">
+          <AuthHeader
+            title="Welcome Back"
+            subTitle="Kindly input your information to obtain access."
+          />
+        </div>
+      </section>
+
+      <section className=" mt-[6.44rem] mb-[4.94rem] ">
+        <div className="flex justify-center ">
+          <form className="">
+            <div className="flex-col  space-y-[1.16rem]">
+              <div className="w-full relative ">
+                <FormInput
+                  type="text"
+                  name="email"
+                  label="email"
+                  hideLabel
+                  placeholder="Email"
+                  style={{
+                    paddingLeft: "40px",
+                    width: "24.625rem",
+                  }}
+                />
+                <div className="absolute left-2 bottom-1/2 translate-y-1/2 ">
+                  <EnvelopeIcon className="w-6 h-6" />
+                </div>
+              </div>
+              <div className="w-full relative">
+                <div className="absolute left-2 bottom-1/2 translate-y-1/2">
+                  <PadlockIcon className="w-6 h-6" />
+                </div>
+                <FormInput
+                  type="password"
+                  label="password"
+                  name="password"
+                  hideLabel
+                  placeholder="Password"
+                  style={{
+                    paddingLeft: "40px",
+                  }}
+                />
+              </div>
+              <div>
+                <a href="#">
+                  <p className="text-[#FFF] text-[0.875remrem] font-[400] leading-normal">
+                    Forgot Password?
+                  </p>
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+
+      <section className="w-full flex justify-center mt-[5rem]">
+        <div className="w-[22.5rem] justify-center space-y-[0.5rem]">
+          <div className="w-full flex items-center justify-center">
+            <AuthButton text="Log in" width="22.5rem" height="3.75rem" />
+          </div>
+          <div className="flex justify-center">
+            <p className="text-[#FFF] text-[1.25rem] font-[500] leading-normal">
+              Or
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <SocialButton text="Google" width="10rem" icon={google} />
+
+            <SocialButton text="Apple" width="10rem" icon={apple} />
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-[13.06rem]">
+        <AuthFooter
+          text="Have no account yet?"
+          linkText="Sign Up"
+          to="Sign Up"
+        />
+      </section>
+
+      {/* <div className="flex-col flex justify-center items-center mt-[6.88rem] w-full">
         <h1 className="text-[#FFF] text-[3.75rem] font-[900] leading-normal ">
           Welcome Back
         </h1>
@@ -84,7 +176,7 @@ export default function Login() {
           </p>
         </div>
 
-      </section>
+      </section> */}
     </>
   );
 }
