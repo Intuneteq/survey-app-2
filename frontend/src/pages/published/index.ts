6b404350bf43ebdd92606716065c8617a084cc00
Intuneteq <tobiolanitori@gmail.com>
2024-01-26 03:57:32 +0000

    feat: checkbox view

--- frontend/src/pages/published/index.ts
+++ frontend/src/pages/published/index.ts
@@ -1,2 +1,3 @@
 export { default as PublishedLayout } from "./PublishedLayout";
 export { default as Intro } from "./Intro";
+export { default as Checkbox } from "./Checkbox";
--- frontend/src/router/published.tsx
+++ frontend/src/router/published.tsx
@@ -1,8 +1,9 @@
 import { RouteObject, Navigate } from "react-router-dom";
 
-import { Intro } from "../pages/published";
+import { Intro, Checkbox } from "../pages/published";
 
 export const publishedRoutes: RouteObject[] = [
   { index: true, element: <Navigate to={"intro"} replace /> },
   { path: "intro", element: <Intro /> },
+  { path: "checkbox", element: <Checkbox /> },
 ];
