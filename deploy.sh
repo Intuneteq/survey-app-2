75d05fe28e7bdcaf677e919582cd6a19eea63ce4
Intuneteq <tobiolanitori@gmail.com>
2024-01-27 12:15:55 +0000

    deploy: new workflow 2

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,8 @@ echo "Deploying application"
 
 #Enter maintenance mode
 
-# (php artisan down --message='The website is been quickly updated. Please tey again in minutes.') || true
-  #update codebase
-
-    # git pull origin v1
-    # git fetch origin master
-    # git reset --hard origin/master
-
     composer2 install
-    # composer2 install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist --no-dev
+
     composer2 dump-autoload
 
     php artisan migrate --force
@@ -27,6 +20,5 @@ echo "Deploying application"
     # script permissions
     chmod -R 777 deploy.sh
 #exit maintainance mode
-# php artisan up
 
 echo "Website deployed"
