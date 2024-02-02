bf4e18ccf76ba0b8722d772b732ce3a6c63aa73a
Christopher <odeniyichristopher7@gmail.com>
2024-02-02 04:55:41 +0000

    latest changes

--- frontend/src/components/molecules/BackgroundContent.tsx
+++ frontend/src/components/molecules/BackgroundContent.tsx
@@ -3,22 +3,21 @@ import Background from "./Background";
 
 type props = {
     children: ReactNode;
+    text:string;
+    subtext:string;
 };
 
-export default function BackgroundContent({ children }: props) {
+export default function BackgroundContent({ children,text, subtext }: props) {
     return (
         <Background>
             <div className="w-full flex justify-center items-center ">
                 <div className=" pt-[0.63rem]">
                     <div className=" text-center w-[51.25rem]">
                         <h3 className="text-[2.5rem] font-[700] leading-normal">
-                            Lorem Ipsum
+                            {text}
                         </h3>
                         <p className="text-[#0000004d] text-[1.25rem] font-[400] leading-[1.5625rem] text-start">
-                            The purpose of an exit survey is to obtain valuable
-                            information that can be used to enhance the
-                            organization's processes, address any issues, and
-                            improve retention rates.
+                            {subtext}
                         </p>
                     </div>
                     <hr />
--- frontend/src/pages/published/Checkbox.tsx
+++ frontend/src/pages/published/Checkbox.tsx
@@ -2,45 +2,8 @@ import BackgroundContent from "../../components/molecules/BackgroundContent";
 
 export default function Checkbox() {
     return (
-<<<<<<< HEAD
-        <BackgroundContent>
+        <BackgroundContent text="Lorem ipsum" subtext="The purpose of an exit survey is to obtain valuable information that can be used to enhance the organization's processes, address any issues, and improve retention rates.">
             
         </BackgroundContent>
-=======
-        <Background>
-            <div className="w-full flex justify-center items-center ">
-                <div className=" pt-[0.63rem]">
-                    <div className=" text-center w-[51.25rem]">
-                        <h3 className="text-[2.5rem] font-[700] leading-normal">
-                            Lorem Ipsum
-                        </h3>
-                        <p className="text-[#0000004d] text-[1.25rem] font-[400] leading-[1.5625rem] text-start">
-                            The purpose of an exit survey is to obtain valuable
-                            information that can be used to enhance the
-                            organization's processes, address any issues, and
-                            improve retention rates.
-                        </p>
-                    </div>
-                    <hr />
-                    <div className=" text-center pt-[0.81rem]">
-                        <h3 className="text-[1.875rem] font-[400] leading-normal text-[#000000db] flex items-center justify-center">
-                            <span className="text-[1.25rem]">1 &gt;</span> Lorem
-                            ipsum dolor sit amet consectetur.
-                        </h3>
-                        <div>
-                            <div>
-                                <CheckedList
-                                    type="radio" // you did not add radio in type FormType
-                                    name="Lorem"
-                                    placeholder="item1"
-                                    label="smh"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Background>
->>>>>>> 24523791b7c4a032c7065944981245e5e0bd5cd8
     );
 }
