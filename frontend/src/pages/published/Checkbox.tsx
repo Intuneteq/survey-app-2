51e583f46697be2da29bf5cacdff8d4a99d436af
Christopher <odeniyichristopher7@gmail.com>
2024-02-02 04:58:24 +0000

    latest changes

--- frontend/src/pages/published/Checkbox.tsx
+++ frontend/src/pages/published/Checkbox.tsx
@@ -2,8 +2,9 @@ import BackgroundContent from "../../components/molecules/BackgroundContent";
 
 export default function Checkbox() {
     return (
-        <BackgroundContent text="Lorem ipsum" subtext="The purpose of an exit survey is to obtain valuable information that can be used to enhance the organization's processes, address any issues, and improve retention rates.">
-            
-        </BackgroundContent>
+        <BackgroundContent
+            text="Lorem ipsum"
+            subtext="The purpose of an exit survey is to obtain valuable information that can be used to enhance the organization's processes, address any issues, and improve retention rates."
+        ></BackgroundContent>
     );
 }
