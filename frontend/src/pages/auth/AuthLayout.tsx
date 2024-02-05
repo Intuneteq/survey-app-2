f5fdeb2ae6dce71a5b7d895bfc703dfe6dcfa3ab
Intuneteq <tobiolanitori@gmail.com>
2024-02-05 02:39:21 +0000

    feat: auth layout redo

--- frontend/src/pages/auth/AuthLayout.tsx
+++ frontend/src/pages/auth/AuthLayout.tsx
@@ -3,20 +3,37 @@ import { Images } from "../../assets";
 import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
-  const { welcome } = Images;
+    const { welcome, Logo } = Images;
 
-  return (
-    <main className="w-full h-screen bg-primary">
-      <div className="flex w-full h-full">
-        <div className="flex justify-center items-end w-[60%] h-full bg-secondary rounded-tr-[2rem] rounded-br-[2rem] xl:rounded-tr-[6.2rem] xl:rounded-br-[6.2rem] py-[3.44rem] px-[6.56rem]">
-          <div className="w-[30rem] h-[25rem] xl:w-[43.125rem] xl:h-[38.125rem] flex justify-center items-center">
-            <img src={welcome} alt="welcome" className="w-full h-full object-contain" />
-          </div>
-        </div>
-        <div className="w-[40%] h-full scroll-vertical">
-          <Outlet />
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="w-full h-screen bg-secondary">
+            <div className="flex w-full h-full">
+                <div className="flex justify-between items-center flex-col w-[48%] h-full bg-primary rounded-tr-[1rem] rounded-br-[1rem] xl:rounded-tr-[6.2rem] xl:rounded-br-[6.2rem] py-[2.44rem] px-[6.56rem]">
+                    <div className="flex justify-start items-center flex-col">
+                        <div className="w-24 h-24 flex justify-center items-center">
+                            <img
+                                src={Logo}
+                                alt="logo"
+                                className="w-full h-full object-contain"
+                            />
+                        </div>
+                        <h1 className="text-[4rem] font-black flex justify-center items-center gap-0">
+                            <span className="text-tertiary">Survey</span>
+                            <span className="text-secondary">Lite</span>
+                        </h1>
+                    </div>
+                    <div className="w-[20rem] h-[15rem] xl:w-[43.125rem] xl:h-[38.125rem] flex justify-center items-center">
+                        <img
+                            src={welcome}
+                            alt="welcome"
+                            className="w-full h-full object-contain"
+                        />
+                    </div>
+                </div>
+                <div className="w-[52%] h-full scroll-vertical">
+                    <Outlet />
+                </div>
+            </div>
+        </main>
+    );
 }
